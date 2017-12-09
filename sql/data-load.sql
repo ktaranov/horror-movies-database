342255f45ca22d9e13585690ae1d2a3c394592eb
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-12-09 10:04:47 -0800

    Moves clean up to separate file

--- sql/data-load.sql
+++ sql/data-load.sql
@@ -1,17 +1,19 @@
-/*
-DROP TABLE IF EXISTS imdb_staging;
-DROP TABLE IF EXISTS movies CASCADE;
-DROP TABLE IF EXISTS movie_cast CASCADE;
-DROP TABLE IF EXISTS movie_genres CASCADE;
-DROP TABLE IF EXISTS movie_locations CASCADE;
-DROP TABLE IF EXISTS actors;
-DROP TABLE IF EXISTS countries;
-DROP TABLE IF EXISTS genres;
-DROP TABLE IF EXISTS locations;
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS ratings;
- */
+/* horror-movies-database Copyright (C) 2017 Jeremiah Peschka
 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 
 CREATE OR REPLACE FUNCTION isnumeric(text) RETURNS boolean AS '
 SELECT $1 ~ ''^[0-9]+$''
